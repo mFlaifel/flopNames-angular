d5857dbc89c3bf176897f9729be99379254037ac
mFlafiel <msf-2oo7@hotmail.com>
2021-03-02 14:12:28 +0200

    handle visibilty of the key

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'flop-names';
   ngOnInit() {}
+
+  keyDisplay = false;
+
+  toggleKey() {
+    this.keyDisplay = !this.keyDisplay;
+  }
+
+  handleDisplay() {
+    if (this.keyDisplay) {
+      return 'visibility:hidden';
+    } else {
+      return '';
+    }
+  }
 }
