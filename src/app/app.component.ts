8f3d08df118a663c3d35d435ed8b096bc8d1871f
mFlafiel <msf-2oo7@hotmail.com>
2021-03-04 16:43:19 +0200

    change current team

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'flop-names';
+  currentTeam = 'Red';
+  teamTextClass = 'redtext';
+
   ngOnInit() {}
 
   keyDisplay = false;
@@ -20,6 +23,15 @@ export class AppComponent {
       return 'visibility:hidden';
     } else {
       return '';
+    }
+  }
+
+  changeTeam(name: string) {
+    this.currentTeam = name;
+    if (name === 'Red') {
+      this.teamTextClass = 'redtext';
+    } else {
+      this.teamTextClass = 'bluetext';
     }
   }
 }
