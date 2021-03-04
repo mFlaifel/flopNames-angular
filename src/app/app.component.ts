3ce146cc578602900355371917920ecf83ad2509
mFlafiel <msf-2oo7@hotmail.com>
2021-03-04 18:31:57 +0200

    add event listener to the tile to change style of the tile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  constructor() {}
   title = 'flop-names';
   currentTeam = 'Red';
   teamTextClass = 'redtext';
+
+  tileClass00: string = '';
+  tileClass01: string = '';
+  tileClass02: string = '';
+  tileClass10: string = '';
+  tileClass11: string = '';
+  tileClass12: string = '';
+  tileClass20: string = '';
+  tileClass21: string = '';
+  tileClass22: string = '';
 
   ngOnInit() {}
 
@@ -28,10 +39,19 @@ export class AppComponent {
 
   changeTeam(name: string) {
     this.currentTeam = name;
+
     if (name === 'Red') {
       this.teamTextClass = 'redtext';
     } else {
       this.teamTextClass = 'bluetext';
     }
+  }
+
+  setTile(x: number, y: number) {
+    console.log(x, y);
+    let xPosition = x.toString();
+    let yPosition = y.toString();
+    // @ts-ignore: Unreachable code error
+    this['tileClass' + xPosition + yPosition] = 'red';
   }
 }
